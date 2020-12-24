41cd1c4b98c4502cca8f181b81ccd780b03148ad
Kuniwak <orga.chem.job@gmail.com>
2020-12-24 16:31:26 +0900

    dotnet new mstest

---
+++ tests/TestRoslynRunner.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRoslynRunner.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
