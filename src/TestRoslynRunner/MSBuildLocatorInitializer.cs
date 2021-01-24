9fc28fde44ce6954b7351020fde51e2f642899a4
Kuniwak <orga.chem.job@gmail.com>
2021-01-24 12:53:44 +0900

    Cosmetic changes

--- src/TestRoslynRunner/MSBuildLocatorInitializer.cs
+++ src/TestRoslynRunner/MSBuildLocatorInitializer.cs
@@ -8,12 +8,12 @@ namespace Dena.CodeAnalysis.Testing
     public static class MSBuidLocatorInitializer
     {
         private static bool _registered;
-        private static object registeredLock = new object();
+        private static object _registeredLock = new object();
 
 
         public static void Setup()
         {
-            lock (registeredLock)
+            lock (_registeredLock)
             {
                 if (_registered)
                 {
@@ -23,8 +23,6 @@ namespace Dena.CodeAnalysis.Testing
                 _registered = true;
             }
 
-            // MSBuildLocatorが何をしているのかはまだ詳しくわかっていない
-            // standaloneのtempleteだと、標準入力を求められる可能性があったので、標準入力を求められないAPIに変更した
             MSBuildLocator.RegisterDefaults();
         }
     }
