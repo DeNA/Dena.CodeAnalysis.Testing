6365279c058ba7193ece1f759cc59f8b650bebcf
Kuniwak <orga.chem.job@gmail.com>
2021-01-24 17:18:05 +0900

    Improve spy tests

--- tests/Dena.CodeAnalysis.Testing.Tests/SpyAnalyzerTests.cs
+++ tests/Dena.CodeAnalysis.Testing.Tests/SpyAnalyzerTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,21 +13,84 @@ namespace Dena.CodeAnalysis.Testing
         public async Task WhenGivenAnyCodes_RecordAllActionHistory()
         {
             var spy = new SpyAnalyzer();
+            var builder = new StringBuilder();
+            var failed = false;
 
             await DiagnosticAnalyzerRunner.Run(spy, ExampleCode.DiagnosticsFree);
 
-            Assert.AreNotEqual(0, spy.CodeBlockActionHistory.Count);
-            Assert.AreNotEqual(0, spy.CodeBlockStartActionHistory.Count);
-            Assert.AreNotEqual(0, spy.CompilationActionHistory.Count);
-            Assert.AreNotEqual(0, spy.CompilationStartActionHistory.Count);
-            Assert.AreNotEqual(0, spy.OperationActionHistory.Count);
-            Assert.AreNotEqual(0, spy.OperationBlockActionHistory.Count);
-            Assert.AreNotEqual(0, spy.OperationBlockStartActionHistory.Count);
-            Assert.AreNotEqual(0, spy.SemanticModelActionHistory.Count);
-            Assert.AreNotEqual(0, spy.SymbolActionHistory.Count);
-            Assert.AreNotEqual(0, spy.SymbolStartActionHistory.Count);
-            Assert.AreNotEqual(0, spy.SyntaxNodeActionHistory.Count);
-            Assert.AreNotEqual(0, spy.SyntaxTreeActionHistory.Count);
+            if (0 == spy.CodeBlockActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.CodeBlockActionHistory));
+            }
+
+            if (0 == spy.CodeBlockStartActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.CodeBlockStartActionHistory));
+            }
+
+            if (0 == spy.CompilationActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.CompilationActionHistory));
+            }
+
+            if (0 == spy.CompilationStartActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.CompilationStartActionHistory));
+            }
+
+            if (0 == spy.OperationActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.OperationActionHistory));
+            }
+
+            if (0 == spy.OperationBlockActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.OperationBlockActionHistory));
+            }
+
+            if (0 == spy.OperationBlockStartActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.OperationBlockStartActionHistory));
+            }
+
+            if (0 == spy.SemanticModelActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.SemanticModelActionHistory));
+            }
+
+            if (0 == spy.SymbolActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.SymbolActionHistory));
+            }
+
+            if (0 == spy.SymbolStartActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.SymbolStartActionHistory));
+            }
+
+            if (0 == spy.SyntaxNodeActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.SyntaxNodeActionHistory));
+            }
+
+            if (0 == spy.SyntaxTreeActionHistory.Count)
+            {
+                failed = true;
+                builder.AppendLine(nameof(spy.SyntaxTreeActionHistory));
+            }
+
+            Assert.IsFalse(failed, builder.ToString());
         }
     }
 }
